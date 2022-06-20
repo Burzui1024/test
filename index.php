000db5f5d6287503465611dd01d64e91a85d5535
VladimirLiux <v.7778111@gmail.com>
2022-06-20 12:30:22 +0300

    test 2

--- index.php
+++ index.php
@@ -1,19 +1,54 @@
 <?php
-class Validator {
+
+class Validator
+{
     private $str;
+
     public function __construct($str)
     {
-        $this->str=$str;
+        $this->str = $str;
     }
 
-    public function validString() {
-        if (substr_count($this->str,'{') == substr_count($this->str,'}'))
-        {
-            return "code true";
+//    public function validString() {
+//        if (substr_count($this->str,'{') == substr_count($this->str,'}'))
+//        {
+//            return "code true";
+//        }
+//        return "code false";
+//    }
+    public function validString()
+    {
+
+        $open = ["{"];
+
+        $stack = array();
+        $this->str = preg_replace('/[a-z0-9]/', '', $this->str);
+
+        for ($i = 0; $i < strlen($this->str); $i++) {
+
+            $current_bracket = $this->str[$i];
+
+            if (in_array($current_bracket, $open))
+
+                array_push($stack, $current_bracket);
+
+            else {
+
+                if (empty($stack)) return "code false";
+
+                $popped_bracket = array_pop($stack);
+
+                if ($popped_bracket === "{" and $current_bracket !== "}")
+
+                    return 'code true';
+            }
+
         }
         return "code false";
+
     }
 }
-$newJob = new Validator('{{lajkdhf{adfa}{}adfasdfadf{}}}');
+
+$newJob = new Validator('}dsfsdfsdf{{}}{');
 print($newJob->validString());
 print ("\n");
